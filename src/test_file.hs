77ad74ed33d869bea11f2d30a4e4241dd609270f
Mshengu27 <tgtshabalala21@gmail.com>
2025-10-08 10:48:09 +0200

    Updated Tutorial and added AuctionValidator.hs

---
+++ src/test_file.hs
@@ -0,0 +1,6 @@
+test-suite test-file
+    type:                exitcode-stdio-1.0
+    hs-source-dirs:      src
+    main-is:             test_file.hs
+    build-depends:       base, HUnit
+    default-language:    Haskell2010
